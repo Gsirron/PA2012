0cc5d46ebe6a8265971cc796c62255b482f312b9
Norris Luong <33255034+Gsirron@users.noreply.github.com>
2020-06-01 04:54:31 +1000

    updated items and validation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Prototype.Areas.Admin.Models;
 using Prototype.Models;
+using SQLitePCL;
 
 namespace Prototype.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly PrototypeContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,PrototypeContext context)
         {
+            _context = context;
             _logger = logger;
         }
         public IActionResult ParticipateClosed()
@@ -58,6 +64,60 @@ namespace Prototype.Controllers
             return View();
         }
 
+        public IActionResult ValidFail(string fail = null)
+        {   
+            if(fail !=null)
+            {
+                ViewData["Fail"] = fail;
+
+                return View();
+            }
+
+            ViewData["Fail"] = "Sorry you are not Eilgiable";
+
+            return View();
+        }
+
+        public async Task<IActionResult> ViewWebsite()
+        {
+            var sitedata = await _context.SiteData.FirstOrDefaultAsync(x => x.SiteData_Active == true);
+
+            ViewData["website"] = sitedata.SiteData_Link;
+
+            return View();
+        }
+        
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNewParticipant(IFormCollection Form)
+        {
+            if (Form["validation"] == "no" ||  Form["validation2"] == "no")
+                {
+                    return RedirectToAction("ValidFail","Home",new {fail =  "FAILED ELIGIBLE" });
+                }
+
+            Participant participant = new Participant();
+            participant.Participant_Email = Form["email"];
+            participant.Participant_Gender = Form["gender"];
+            participant.Participant_Age = int.Parse(Form["age"]);
+            participant.Participant_Phase = int.Parse(Form["phase"]);
+            participant.Participant_Group = Form["group"];
+
+            if (!ParticipantExists(participant))
+            {
+                _context.Add(participant);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(ViewWebsite));
+            }
+            return RedirectToAction("ValidFail", "Home", new { fail = "Already taken Survey" });
+
+        }
+
+        private bool ParticipantExists(Participant participant)
+        {
+            return _context.Participant.Any(e => e.Participant_Email == participant.Participant_Email && e.Participant_Group == participant.Participant_Group &&
+            e.Participant_Phase == participant.Participant_Phase);
+        }
         public IActionResult Report()
         {
             ViewData["Message"] = "Application's Reports Page.";
